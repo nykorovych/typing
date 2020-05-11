ffdd94a35e575b2a2d8eb5f54ed9eb015c32fefe
ny.iuriii@gmail.com <ny.iuriii@gmail.com>
2020-05-11 17:25:25 +0200

    reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { address, lorem} from 'faker';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,16 +9,27 @@ import { address, lorem} from 'faker';
 })
 export class AppComponent {
   randomSentance = lorem.sentence();
-  typedString =''
-
+  typedString ='';
+  
 
   onInput (input:any) {
     this.typedString = input
+    if (input.length === this.randomSentance.length) {
+      this.reset()
+
+    }
     
-  }
+  };
 
   compare(letter: string, enteredLetter: string) {
     if (!enteredLetter) return 'pennding'
+    
     return letter === enteredLetter ? 'correct' : 'wrong'
+    this.reset()
+  }
+
+  reset() {
+    this.typedString = '';
+    this.randomSentance =lorem.sentence()
   }
 }
